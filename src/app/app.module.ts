72dbdc7b0808e7b9018c0733d6e0130956af05f1
adriantr0808 <adriantr.gt@gmail.com>
2021-11-15 14:24:13 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LibreriaComponent } from './components/libreria/libreria.component';
 import { ComicComponent } from './components/comic/comic.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { appRoutingProviders, routing } from './app.routing';
+
 
 @NgModule({
   declarations: [
@@ -18,9 +20,11 @@ import { MenuComponent } from './components/menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/comic/comic.component.ts
+++ src/app/components/comic/comic.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Comic } from 'src/app/models/comic';
 @Component({
   selector: 'app-comic',
   templateUrl: './comic.component.html',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComicComponent implements OnInit {
 
+  @Input() comic!: Comic;
   constructor() { }
 
   ngOnInit(): void {
